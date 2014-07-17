382d1516f4ce7887606e49aa865ed450d0e477d3
David Fisher <fisherds@rose-hulman.edu>
2014-07-17 12:33:41 -0400

    use username for the sort order

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from endpoints_proto_datastore.ndb.model import EndpointsModel
 
 class Student(EndpointsModel):
     """ Student. """
-    _message_fields_schema = ("entityKey", "first_name", "last_name", "rose_username", "team") 
+    _message_fields_schema = ("entityKey", "first_name", "last_name", "rose_username", "team")
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
     rose_username = ndb.StringProperty()
@@ -12,12 +12,12 @@ class Student(EndpointsModel):
 
 class Assignment(EndpointsModel):
     """ Assignment. """
-    _message_fields_schema = ("entityKey", "name") 
+    _message_fields_schema = ("entityKey", "name")
     name = ndb.StringProperty()
 
 class GradeEntry(EndpointsModel):
     """ Score for a student on an assignment. """
-    _message_fields_schema = ("entityKey", "score", "student_key", "assignment_key") 
+    _message_fields_schema = ("entityKey", "score", "student_key", "assignment_key")
     score = ndb.IntegerProperty()
     student_key = ndb.KeyProperty(kind=Student)
     assignment_key = ndb.KeyProperty(kind=Assignment)
