45882071cabfc525ebca7ad921229a4589a1ae97
Matt Boutell <boutell@gmail.com>
2014-07-17 10:48:10 -0400

    Make into Endpoints model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
+from endpoints_proto_datastore.ndb.model import EndpointsModel
 
-class Student(ndb.Model):
+class Student(EndpointsModel):
     """ Student. """
     _message_fields_schema = ("entityKey", "first_name", "last_name", "rose_username", "team") 
     first_name = ndb.StringProperty()
@@ -9,12 +10,12 @@ class Student(ndb.Model):
     team = ndb.StringProperty()
 
 
-class Assignment(ndb.Model):
+class Assignment(EndpointsModel):
     """ Assignment. """
     _message_fields_schema = ("entityKey", "name") 
     name = ndb.StringProperty()
 
-class GradeEntry(ndb.Model):
+class GradeEntry(EndpointsModel):
     """ Score for a student on an assignment. """
     _message_fields_schema = ("entityKey", "score", "student_key", "assignment_key") 
     score = ndb.IntegerProperty()
